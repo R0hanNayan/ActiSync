e5cfb9fd13a2696774703ecdf9316150646f6a0d
Rohan Nayan <rohannayan405@gmail.com>
2023-10-10 20:56:49 +0530

    Register Page CSS Changes

--- client/src/scenes/register/Register.jsx
+++ client/src/scenes/register/Register.jsx
@@ -58,33 +58,35 @@ function Register() {
     }
 
     return (
-        <div className="RegisterPage">
-            <div className="RegisterPage-Container">
-                <h1 id="RegTitle">Welcome to ActiSync</h1>
-                <div className="RegisterPage-inputs">
-                    <h2 id="signUp">Sign Up</h2>
-                    <input id="userName" type="username" name="userName" value={user.userName} placeholder="Username" onChange={handleChange} />
-                    <input id="email" type="email" name="email" value={user.email} placeholder="Email Id" onChange={handleChange} />
-                    <input id="password" type="password" name="password" value={user.password} placeholder="Password" onChange={handleChange} />
-                    <div className="userDetails">    
-                        <label id="gender">
-                            <select id="gender-box" name="gender" value={user.gender} onChange={handleChange}>
-                                <option value="">--Gender--</option>
-                                <option value="Male">Male</option>
-                                <option value="Female">Female</option>
-                                <option value="Others">Others</option>
-                            </select>
-                        </label>
-                        <input id="age" type="number" name="age" min={1} max={150} value={user.age} placeholder="Age" onChange={handleChange} />
-                        <input id="height" type="number" name="height" min={0} max={5} step={0.01} value={user.height} placeholder="Height in meters" onChange={handleChange} />
-                        <input id="weight" type="number" name="weight" min={0} max={200} step={0.1} value={user.weight} placeholder="Weight in Kgs" onChange={handleChange} />
+        <div className="Register">
+            <div className="RegisterPage">
+                <div className="RegisterPage-Container">
+                    <h1 id="RegTitle">Welcome to ActiSync</h1>
+                    <div className="RegisterPage-inputs">
+                        <h2 id="signUp">Sign Up</h2>
+                        <input id="userName" type="username" name="userName" value={user.userName} placeholder="Username" onChange={handleChange} />
+                        <input id="email" type="email" name="email" value={user.email} placeholder="Email" onChange={handleChange} />
+                        <input id="password" type="password" name="password" value={user.password} placeholder="Password" onChange={handleChange} />
+                        <div className="userDetails">    
+                            <label id="gender">
+                                <select id="gender-box" name="gender" value={user.gender} onChange={handleChange}>
+                                    <option value="">--Gender--</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                                    <option value="Others">Others</option>
+                                </select>
+                            </label>
+                            <input id="age" type="number" name="age" min={1} max={150} value={user.age} placeholder="Age" onChange={handleChange} />
+                            <input id="height" type="number" name="height" min={0} max={5} step={0.01} value={user.height} placeholder="Height in meters" onChange={handleChange} />
+                            <input id="weight" type="number" name="weight" min={0} max={200} step={0.1} value={user.weight} placeholder="Weight in Kgs" onChange={handleChange} />
+                        </div>
+                        <button id="RegBtn1" type="submit" onClick={registerUser}>Sign Up</button>
+                        <button id="RegBtn2" type="submit" onClick={() => { navigate("/") }}>Already Have an Account?</button>
                     </div>
-                    <button id="RegBtn1" type="submit" onClick={registerUser}>Sign Up</button>
-                    <button id="RegBtn2" type="submit" onClick={() => { navigate("/") }}>Already Have an Account?</button>
                 </div>
-            </div>
-            <div className="RegisterPage-left">
-                <img id="regImg" src="/assets/regImg.jpg" alt="Register" />
+                <div className="RegisterPage-Right">
+                    <img id="regImg" src="/assets/regImg.jpg" alt="Register" />
+                </div>
             </div>
         </div>
     )
