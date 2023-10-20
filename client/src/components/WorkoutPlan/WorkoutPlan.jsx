4ce653ac6fd0865b508df3e215f5ff35df7c93df
Rohan Nayan <rohannayan405@gmail.com>
2023-10-20 19:50:13 +0530

    Workout Plan Card Changes

--- client/src/components/WorkoutPlan/WorkoutPlan.jsx
+++ client/src/components/WorkoutPlan/WorkoutPlan.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import URL from "../../URL";
 import './workoutplan.css';
 
-function WorkoutPlan({user}){
+function WorkoutPlan({ user }) {
     const date = new Date();
     const [workout, setWorkout] = useState(
         {
@@ -15,6 +15,8 @@ function WorkoutPlan({user}){
             duration: null
         }
     )
+
+    const [isCardio, setIsCardio] = useState(false);
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -36,29 +38,45 @@ function WorkoutPlan({user}){
                     'Content-Type': 'application/json'
                 }
             })     //Sending data to Backend
-            .then((res) =>{
+                .then((res) => {
                     alert("Success!");
                     console.log(res.savedWorkout);
                 }
-            )
+                )
         } else {
             alert("Invalid Input");
         }
     }
-    return(
+    return (
         <div className="WorkoutPlan">
             {/* <input id="date" type="date" name="day" value={workout.day} placeholder="Day" onChange={handleChange} /> */}
             <div className="exercise-Type">
-                <input id="exerciseType" type="text" name="exerciseType" value={workout.exerciseType} placeholder="Exercise Type" onChange={handleChange} />
+                {/* <input id="exerciseType" type="text" name="exerciseType" value={workout.exerciseType} placeholder="Exercise Type" onChange={handleChange} /> */}
+                <select id="exerciseType" name="exerciseType" value={workout.exerciseType} onChange={handleChange}>
+                    <option value="">Please Select an Option</option>
+                    <option value="Chest">Chest</option>
+                    <option value="Triceps">Triceps</option>
+                    <option value="Biceps">Biceps</option>
+                    <option value="Shoulder">Shoulder</option>
+                    <option value="Back">Back</option>
+                    <option value="Legs">Legs</option>
+                    <option value="Cardio">Cardio</option>
+                </select>
             </div>
-            <div className="type-1">
-                <input id="sets" type="Number" name="sets" value={workout.sets} placeholder="Sets" onChange={handleChange} />
-                <input id="reps" type="Number" name="reps" value={workout.reps} placeholder="Reps" onChange={handleChange} />
-            </div>
-            <p id="WoroutPlan-or">Or</p>
-            <div className="type-2">
-                <input id="duration" type="Number" name="duration" value={workout.duration} placeholder="Minutes" onChange={handleChange} />
-            </div>
+            {
+                workout.exerciseType === "Cardio" ? (
+                    <div className="type-2">
+                        <input id="duration" type="Number" name="duration" value={workout.duration} placeholder="Minutes" onChange={handleChange} />
+                    </div>
+                ) : workout.exerciseType === "" ? (
+                        null
+                    ) : (
+                        <div className="type-1">
+                            <input id="sets" type="Number" name="sets" min={1} value={workout.sets} placeholder="Sets" onChange={handleChange} />
+                            <input id="reps" type="Number" name="reps" min={1} value={workout.reps} placeholder="Reps" onChange={handleChange} />
+                        </div>
+                    )
+            }
             <button id="WoroutPlan-Save" type="submit" onClick={saveWorkout}>Save!</button>
         </div>
     )
